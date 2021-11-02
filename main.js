a8f5376cc7182894b81c0de4e148dbf114f94869
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-01 19:16:47 -0600

    Successful call to the API

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 let buttons = [...document.querySelectorAll(".btn")];
+let arr = []
+
+const randomHandler = async () => {
+    let apiURL = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+    
+    try{
+        let response = await fetch(apiURL)
+        let data = await response.json()
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+const chosenHandler = () => {
+    console.log("Chosen")
+}
 
 const clickHandler = (e) => {
     e.preventDefault()
     
     if(e.target.id == "chosen"){
-        console.log("Chosen")
+        chosenHandler()
     }else{
-        console.log("Random")
+        randomHandler()
     }
 }
 
