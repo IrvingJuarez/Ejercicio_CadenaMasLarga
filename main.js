8dae74a5a6ee6e8603ffc4a9f8714cfec9be70a2
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-01 18:56:29 -0600

    The btns are already identified

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+let buttons = [...document.querySelectorAll(".btn")];
+
+const clickHandler = (e) => {
+    e.preventDefault()
+    
+    if(e.target.id == "chosen"){
+        console.log("Chosen")
+    }else{
+        console.log("Random")
+    }
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", clickHandler)
+})
